ab2e2201c8053e51890d3fdb355ecdd718e8e25f
WahajRashid1020 <wahaj1020@gmail.com>
2022-08-31 23:54:47 +0500

    updateagain

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/todos", todosRoutes);
-const mongodb =
-  "mongodb+srv://WahajRashid:Wahaj1812214!@items-database.8mzu1.mongodb.net/mern-database?retryWrites=true&w=majority";
 
 app.get("/", (req, res) => {
   res.send("Welcome to server");
 });
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true })
+  .connect(process.env.mongodb, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`server is running on port ${PORT}`);
